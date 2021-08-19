fbb3bd7800ce6a973a1cabc340c0bdd70a57d778
JichouP <akkunn100@gmail.com>
2021-08-19 15:31:50 +0900

    -v --verison

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 // child.execFileSync(`${__dirname}/presets/git-hooks/husky/base/init.sh`);
 
+import { program } from 'commander';
+import { version } from '../package.json';
 import { processing } from './processing';
 import { prompt } from './prompt';
 import { promptValidator } from './zod';
+
+// commander の program.versionにpackage.jsonのversionを設定
+program.version(version, '-v --version').parse();
 
 prompt()
   .then((res) => {
